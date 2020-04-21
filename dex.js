e0e39328a2b452fb00e30ab1136f6e508a26bc69
Sigmatecture <64104812+Sigmatecture@users.noreply.github.com>
2020-04-21 19:13:41 -0400

    working prototype of level changes

---
+++ dex.js
@@ -0,0 +1,35 @@
+// Format taken from https://github.com/smogon/pokemon-showdown/blob/125fe31d06ad098a3681631999db5fe6dbcd9f4e/data/pokedex.js
+
+/**@type {{[k: string]: TemplateData}} */
+let BattlePokedex = {
+	bellossom: {
+		num: 182,
+		level: 84,
+	},
+	solrock: {
+		num: 338,
+		level: 84,
+	},
+	sylveon: {
+		num: 700,
+		level: 80,
+	},
+	araquanid: {
+		num: 752,
+		level: 82,
+	},
+	corviknightgmax: {
+		num: 823,
+		level: 80,
+	},
+	hatterenegmax: {
+		num: 858,
+		level: 80,
+	},
+	snom: {
+		num: 872,
+		level: 99,
+	},
+};
+
+module.exports = BattlePokedex;
---
+++ updater.js
@@ -0,0 +1,63 @@
+var dex =  require('./dex.js');
+var fs = require('fs');
+
+/* Utility function to read stats into JSON.
+  @input    csv       CSV file path of level changes for each species. 
+                      Must contain two columns: 'Species' and 'Change'.
+  @return   updates   JSON array of level change for each species.
+                      Fields are 'species' and 'change'. 
+*/
+function formatUpdates(csv){
+
+  // Can replace with readSync() wrapper from ../lib/fs.ts
+  let input = fs.readFileSync(csv);
+  // console.log("CSV level updates:\n______________\n" + input.toString() + "\n______________")
+  let lines=input.toString().split(/\b\s+/); 
+  let result = [];
+  let cols=lines[0].split(",");
+
+  for (let i=1; i<lines.length; i++) {
+    let currLine=lines[i].split(",");
+    result.push({
+      species: currLine[0].toLowerCase(),
+      change: parseInt(currLine[1])
+    });
+  }
+  // Level changes formatted into JSON
+  result = JSON.stringify(result);
+  updates = JSON.parse(result);
+
+  return updates;
+
+}
+
+/* Update levels of species. Wraps formatUpdates().
+  @input  csv       Path of csv file containing level updates. 
+          dexLevels JSON object of every species and its current level
+          reverse   Makes each level change the opposite sign (for reversing a previous change).
+  @return levels    Updated levels as a JavaScript object.
+*/
+function updateLevels(csv, dexLevels=dex, reverse = false) {
+  let sign = (reverse ? -1 : 1);
+  // Call JSON parser on manually-updated CSV file
+  changes = formatUpdates(csv);
+  changes.forEach( function(entry) {
+    dexLevels[entry.species].level += entry.change * sign
+  });
+
+  return dexLevels
+
+}
+
+updates = formatUpdates(('./levelChanges.csv'));
+console.log("Changes to levels:\n" + JSON.stringify(updates, undefined, 1));
+
+// The main function call
+console.log("Initial levels: ");
+console.log(dex);
+updated = updateLevels('./levelChanges.csv');
+console.log("Updated levels: ");
+console.log(updated);
+
+
+
